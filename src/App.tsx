b27c59f36ee0f5c2b4d97199072b5aa52f82b0a3
Савелий Шовгур <savelijsovgur@MacBook-Air-Savelij.local>
2022-10-25 16:42:53 +0300

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+
+
+import React from "react";
+
 //@ts-ignore
 import styles from "./App.module.css";
+// import SignIn from "./Pages/SignIn";
+import SignUp from "./Components/Pages/SignUp";
 
 const App = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+        <SignUp />
+    </div>
+  );
 };
 
 export default App;
+
