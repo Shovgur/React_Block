a3089999ea95137a6b233ad292ee7dc0d55fd091
Савелий Шовгур <savelijsovgur@MacBook-Air-Savelij.local>
2022-10-30 18:40:10 +0300

    adeed homewrok

--- src/Assets/icons/index.ts
+++ src/Assets/icons/index.ts
@@ -1,2 +1,6 @@
 export * from "./BurgerClosedIcon";
 export * from "./CloseIcon";
+export { LikeIcon } from './Likeicon'
+export { DislikeIcon } from './DislikeIcon'
+export { BookmarkIcon } from './BookmarkIcon'
+export { SettingsIcon } from './SettingIcon'
--- src/Components/Pages/SignIn/SignIn.tsx
+++ src/Components/Pages/SignIn/SignIn.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import FormContainer from "../..//FormContainer";
+import FormContainer from "../../FormContainer";
 import Input from "../../Input";
 //@ts-ignore
 import styles from "./SignIn.module.css";
