81f4e80d744e6bc3b16e3be99d7a9a5c275b465c
Савелий Шовгур <savelijsovgur@MacBook-Air-Savelij.local>
2022-11-10 17:19:14 +0300

    fix

--- src/Assets/icons/index.ts
+++ src/Assets/icons/index.ts
@@ -1,8 +1,8 @@
 export * from "./BurgerClosedIcon";
 export * from "./CloseIcon";
 export { LikeIcon } from "./LikeIcon";
-export { DislikeIcon } from "./DisLikeIcon";
-export { BookmarkIcon } from "./BookMarkIcon";
+export { DislikeIcon } from "./DislikeIcon";
+export { BookmarkIcon } from "./BookmarkIcon";
 export { SettingsIcon } from "./SettingsIcon";
 export { SunIcon } from "./SunIcon";
 export { MoonIcon } from "./MoonIcon";
