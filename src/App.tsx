5f62e9ace7a8bf2bc86bada38e24f4da15175843
Савелий Шовгур <you@example.com>
2022-11-17 01:14:51 +0300

    sad

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Provider, useDispatch, useSelector } from "react-Redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 
 import ThemeProvider from "./Context/Theme";
 import { Theme } from "./Constants/@types";
